b2d60baddc7763c02d1f6451e491351d0b380c4d
Asen <asen.kamboshev@edited.com>
2021-11-02 10:54:47 +0200

    t

--- glamorous/glamorous/spiders/glamorous.py
+++ glamorous/glamorous/spiders/glamorous.py
@@ -1,5 +1,7 @@
+from scrapy import Request
 from scrapy.spiders import Spider, CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+import json
 
 class Mixin:
     retailer = 'glamorous'
@@ -9,7 +11,15 @@ class Mixin:
 
 class GlomorousParseSpider(Mixin, Spider):
     name = Mixin.retailer + '-parse'
+
     def parse(self, response, **kwargs):
+
+        data = response.css('script[id="ProductJson-product-template"]::text').get()
+        if data:
+            data_json = json.loads(data)
+
+        
+        product_id = data_json['id']
         title_desc = response.css('h2.name::text').get()
         price = response.css('span[itemprop="price"]::text').get()
         sizes = response.css('option::attr(value)').getall()
@@ -17,19 +27,60 @@ class GlomorousParseSpider(Mixin, Spider):
         if not title_desc:
             return
         sizes = [size for size in sizes if len(size)<=2]
-        yield {
-            'title_desc': title_desc,
-            'price': price,
-            'sizes': sizes,
-            'availability': availability
-        }
 
+        yield {
+            "_values": {
+                "retailer_sku": "",
+                "gender": "",
+                "category": [],
+                "brand": "",
+                "url": "",
+                "retailer": "",
+                "name": "",
+                "description": [],
+                "care": [],
+                "image_urls": {
+                    "colour": [
+                        "https://"
+                    ],
+                    "colour": [
+                        "https://"
+                    ],
+                    "colour": [
+                        "https://"
+                    ],
+                    "colour": [
+                        "https://"
+                    ]
+                    }
+                }
+            }
 
 class GlomorousCrawlSpider(Mixin, CrawlSpider):
     name = Mixin.retailer + '-crawl'
     parse_spider = GlomorousParseSpider()
 
-    rules = [Rule(LinkExtractor(allow='collections/'), callback='parse_item', follow=True)]
+    listings_css = [
+        '[id="megamenu-header-menu2"] ul',
+        '[id=bc-sf-filter-bottom-pagination]',
+    ]
+
+    products_css = [
+        'div[class="bc-sf-filter-product-item-inner"]',
+    ]
+
+    rules = [
+        Rule(LinkExtractor(restrict_css=listings_css), callback='parse'),
+        Rule(LinkExtractor(restrict_css=products_css), callback='parse_item')
+    ]
+
+    # rules = [
+    #     Rule(LinkExtractor(allow='collections/'), callback='parse_item', follow=True)
+    # ]
+
+
+    def parse(self, response, **kwargs):
+        return super()._parse(response, **kwargs)
 
     def parse_item(self, response, **kwargs):
         return self.parse_spider.parse(response)
